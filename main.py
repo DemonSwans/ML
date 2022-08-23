ec32d5028994d6675db00c88f01fe81da0f854b3
DemonSwans <swannsyswan@gmail.com>
2022-08-23 15:08:28 +0200

    23

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from time import sleep
 
 # Make NumPy printouts easier to read.
 np.set_printoptions(precision=3, suppress=True)
@@ -11,7 +12,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 print(tf.__version__)
 
-column_names = ["Number1", "Number2", "Number3"]
+column_names = ["Number1","znak", "Number2", "Number3"]
 
 with open("liczby.txt",'r') as file:
    data=[]
@@ -22,7 +23,7 @@ with open("liczby.txt",'r') as file:
       data.append(line)
 
 data = np.array(data)
-data = pd.DataFrame(data,columns=["Number1","Plus","Minus", "Number2", "Number3"])
+data = pd.DataFrame(data,columns=["Number1","znak", "Number2", "Number3"])
 
 train_dataset = data.sample(frac=0.8, random_state=0)
 test_dataset = data.drop(train_dataset.index)
@@ -47,35 +48,38 @@ with np.printoptions(precision=2, suppress=True):
   print()
   print('Normalized:', normalizer(first).numpy())
 
-sum_normalizer = layers.Normalization(input_shape=[4,],axis=None)
+sum_normalizer = layers.Normalization(input_shape=[3,],axis=None)
 sum_normalizer.adapt(train_features)
 
 sum_model = tf.keras.Sequential([
    sum_normalizer,
-
+    layers.Dense(units=128, activation="relu"),
+    layers.Dense(units=128, activation="relu"),
+    layers.Dense(units=128, activation="relu"),
+    layers.Dense(units=128, activation="relu"),
    layers.Dense(units=1)
 ])
 
 sum_model.summary()
-
+sleep(3)
 print(sum_model.predict(train_features[:10]))
 
 sum_model.compile(
-   optimizer=tf.optimizers.Nadam(learning_rate=0.001),
+   optimizer=tf.optimizers.Adam(),
    loss="mean_absolute_error")
 
-sum_model.fit(train_features,train_labels,epochs=100,validation_split = 0.2)
+sum_model.fit(train_features,train_labels,epochs=200,validation_split = 0.2, batch_size= 1024)
 print("2-7")
-print(sum_model.predict([2,0,1,7])[0])
+print(sum_model.predict([2,0,7])[0])
 print("76+7")
-print(sum_model.predict([76,1,0,7])[0])
+print(sum_model.predict([76,1,17])[0])
 print("213-200")
-print(sum_model.predict([213,0,1,200])[0])
+print(sum_model.predict([213,0,200])[0])
 print("49+51")
-print(sum_model.predict([49,1,0,51])[0])
+print(sum_model.predict([49,1,51])[0])
 print("1231+3124")
-print(sum_model.predict([1231,1,0,3124])[0])
+print(sum_model.predict([1231,1,3124])[0])
 print("1-1111")
-print(sum_model.predict([1,0,1,1111])[0])
+print(sum_model.predict([1,0,1111])[0])
 print("12-3")
-print(sum_model.predict([12,0,1,3])[0])
+print(sum_model.predict([12,0,3])[0])
--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 with open("liczby.txt",'w') as file:
-   for i in range(101):
-      for x in range(101):
-         file.write(f'{i} {1} {0} {x} {i + x}\n')
-         file.write(f'{i} {0} {1} {x} {i - x}\n')
+   for i in range(1001):
+      for x in range(1001):
+         file.write(f'{i} {1} {x} {i + x}\n')
+         file.write(f'{i} {0} {x} {i - x}\n')
 
 with open("liczby.txt",'r') as file:
    data=[]
